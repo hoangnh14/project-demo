9b28b7bd6a978773176af549ea487ac25d9f3d29
HoangNH <hoangnhhe130062@fpt.edu.vn>
2020-07-24 16:49:13 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Index from "./Layouts";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Index />
   </React.StrictMode>,
   document.getElementById('root')
 );
